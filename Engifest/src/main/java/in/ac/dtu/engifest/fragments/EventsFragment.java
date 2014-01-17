415876cc919fa5c4da5ed65d0d4b8d36d6900626
Umair Khan <omerjerk@gmail.com>
2014-01-17 23:10:28 +0530

    Implement ViewPager layout
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- Engifest/src/main/java/in/ac/dtu/engifest/fragments/EventsFragment.java
+++ Engifest/src/main/java/in/ac/dtu/engifest/fragments/EventsFragment.java
@@ -3,15 +3,19 @@ package in.ac.dtu.engifest.fragments;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.afollestad.cardsui.Card;
-import com.afollestad.cardsui.CardAdapter;
 import com.afollestad.cardsui.CardHeader;
 import com.afollestad.cardsui.CardListView;
+import com.astuetz.PagerSlidingTabStrip;
 
 import in.ac.dtu.engifest.EventsCardAdapter;
 import in.ac.dtu.engifest.MainActivity;
@@ -27,6 +31,22 @@ public class EventsFragment extends Fragment {
      * fragment.
      */
     private static final String ARG_SECTION_NUMBER = "section_number";
+
+    /**
+     * The number of pages to show.
+     */
+    private static final int NUM_PAGES = 3;
+
+    /**
+     * The pager widget, which handles animation and allows swiping horizontally to access previous
+     * and next wizard steps.
+     */
+    private ViewPager mPager;
+
+    /**
+     * The pager adapter, which provides the pages to the view pager widget.
+     */
+    private PagerAdapter mPagerAdapter;
 
     /**
      * Returns a new instance of this fragment for the given section
@@ -47,15 +67,44 @@ public class EventsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_event, container, false);
-        CardListView eventList = (CardListView) rootView.findViewById(android.R.id.list);
 
-        EventsCardAdapter adapter = new EventsCardAdapter(getActivity());
-        adapter.add(new CardHeader("Events"));
-        adapter.add(new Card("One", "Example 1"));
-        adapter.add(new Card("Two", "Example 2"));
-        adapter.add(new Card("Three", "Example 3"));
-        eventList.setAdapter(adapter);
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = (ViewPager) rootView.findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+
+        // Bind the tabs to the ViewPager
+        PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) rootView.findViewById(R.id.tabs);
+        tabs.setViewPager(mPager);
+
         return rootView;
+    }
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class ScreenSlidePagerAdapter extends FragmentPagerAdapter {
+
+        String[] tabsItems = {"Day I", "Day II", "Day III"};
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabsItems[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new EventsSlidingFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 
     @Override
@@ -63,5 +112,25 @@ public class EventsFragment extends Fragment {
         super.onAttach(activity);
         ((MainActivity) activity).onSectionAttached(
                 getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+
+    private class EventsSlidingFragment extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View v = inflater.inflate(R.layout.fragment_sliding_events, container, false);
+
+            CardListView eventList = (CardListView) v.findViewById(android.R.id.list);
+
+            EventsCardAdapter adapter = new EventsCardAdapter(getActivity());
+            adapter.add(new CardHeader("Events"));
+            adapter.add(new Card("One", "Example 1"));
+            adapter.add(new Card("Two", "Example 2"));
+            adapter.add(new Card("Three", "Example 3"));
+            eventList.setAdapter(adapter);
+            return v;
+        }
     }
 }
